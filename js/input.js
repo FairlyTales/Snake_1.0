d97c903a43166e9c7211a4442925736e41eed5f3
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-05-17 11:51:17 +0300

    added restart game on space and enter keys

--- js/input.js
+++ js/input.js
@@ -1,5 +1,5 @@
-let inputDirection = { x: 0, y: 0 };
-let lastInputDirection = { x: 0, y: 0};
+let inputDirection = {x: 0, y: 0};
+let lastInputDirection = {x: 0, y: 0};
 let modalShown = true;
 
 export let speedUp = false;
@@ -10,62 +10,81 @@ export function getInputDirection() {
 }
 
 // controls
-window.addEventListener('keydown', e => {
+window.addEventListener("keydown", (e) => {
   switch (e.key) {
-    case 'w':
-    case 'W':
-    case 'ц':
-    case 'Ц':
-    case 'ArrowUp':
+    case "w":
+    case "W":
+    case "ц":
+    case "Ц":
+    case "ArrowUp":
       // remove pre-game overlay
-      if(modalShown === true) document.querySelector('.modal--before-game').classList.add('modal--hide');
+      if (modalShown === true)
+        document
+          .querySelector(".modal--before-game")
+          .classList.add("modal--hide");
 
       // preventing snake from reversing
-      if (lastInputDirection.y === 1) break 
-
-      // accelerate if the direction button is pushed twice 
+      if (lastInputDirection.y === 1) break;
+      // accelerate if the direction button is pushed twice
       else if (lastInputDirection.y === -1) speedUp = true;
 
       // update the direction
-      inputDirection = {x: 0, y: -1 }; 
+      inputDirection = {x: 0, y: -1};
       break;
 
-    case 's':
-    case 'S':
-    case 'ы':
-    case 'Ы':
-    case 'ArrowDown':
-      if(modalShown === true) document.querySelector('.modal--before-game').classList.add('modal--hide');
-      if (lastInputDirection.y === -1) break
+    case "s":
+    case "S":
+    case "ы":
+    case "Ы":
+    case "ArrowDown":
+      if (modalShown === true)
+        document
+          .querySelector(".modal--before-game")
+          .classList.add("modal--hide");
+      if (lastInputDirection.y === -1) break;
       else if (lastInputDirection.y === -1) speedUp = true;
-      inputDirection = {x: 0, y: 1 };
+      inputDirection = {x: 0, y: 1};
       break;
 
-    case 'a':
-    case 'A':
-    case 'ф':
-    case 'Ф':
-    case 'ArrowLeft':
-      if(modalShown === true) document.querySelector('.modal--before-game').classList.add('modal--hide');
-      if (lastInputDirection.x === 1) break
+    case "a":
+    case "A":
+    case "ф":
+    case "Ф":
+    case "ArrowLeft":
+      if (modalShown === true)
+        document
+          .querySelector(".modal--before-game")
+          .classList.add("modal--hide");
+      if (lastInputDirection.x === 1) break;
       else if (lastInputDirection.x === -1) speedUp = true;
-      inputDirection = {x: -1, y: 0 };
+      inputDirection = {x: -1, y: 0};
       break;
 
-    case 'd':
-    case 'D':
-    case 'в':
-    case 'В':
-    case 'ArrowRight':
-      if(modalShown === true) document.querySelector('.modal--before-game').classList.add('modal--hide');
-      if (lastInputDirection.x === -1) break
+    case "d":
+    case "D":
+    case "в":
+    case "В":
+    case "ArrowRight":
+      if (modalShown === true)
+        document
+          .querySelector(".modal--before-game")
+          .classList.add("modal--hide");
+      if (lastInputDirection.x === -1) break;
       else if (lastInputDirection.x === 1) speedUp = true;
-      inputDirection = {x: 1, y: 0 };
+      inputDirection = {x: 1, y: 0};
+      break;
+
+    // game restart on "Enter" and "Space"
+    case "Enter":
+    case " ":
+      if (document.querySelector(".modal--after-game").classList.contains('modal--show')) {
+        location.reload();
+      }
       break;
   }
-})
+});
 
 // game restart
-document.querySelector('#restart-btn').addEventListener('click', e => {
+document.querySelector("#restart-btn").addEventListener("click", (e) => {
   location.reload();
-})
+});
